951165022a05ac0b238b50483704a358f8ea0edb
samsonatere <samsonatere24@gmail.com.com>
2021-11-16 12:32:25 +0100

    requirement

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class CustomUser(AbstractUser):
     age = models.PositiveIntegerField(null=True, blank=False)
     role = models.CharField(max_length=20, null=True, blank=True)
+    image = CloudinaryField(default="profile1.png", null=True, blank=True)
