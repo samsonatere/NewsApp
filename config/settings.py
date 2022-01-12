034ec4587c4ead5970665a13184dc589af5bf84c
Anthony <tonyudeagbala@gmail.com>
2022-01-12 09:48:20 +0000

    Add DB settings

--- config/settings.py
+++ config/settings.py
@@ -95,7 +95,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-"default": env.dj_db_url("DATABASE_URL")
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
