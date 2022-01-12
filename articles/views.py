9ecefb004b83901d208ebb63b51e838c078751f3
Anthony <tonyudeagbala@gmail.com>
2022-01-12 17:53:38 +0000

    Add get_context_data to ArticleDetailView

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import UpdateView, DeleteView, CreateView
 from django.urls import reverse_lazy
+from accounts.forms import NewCommentForm
 from .models import Article, Comment
 
 class ArticleListView(ListView):
@@ -11,6 +12,17 @@ class ArticleListView(ListView):
 class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = 'article_detail.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+
+        article_comments = Comment.objects.filter(article=self.get_object()).order_by('-date')
+        if article_comments:
+            data['comments'] = article_comments
+        if self.request.user.is_authenticated:
+            data['comment_form'] = NewCommentForm(instance=self.request.user)
+
+        return data
 
 class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
